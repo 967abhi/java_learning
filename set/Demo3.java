f96bfe54c16cc87f10f7210c55f09275b6e398b9
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2026-08-28 00:37:47 +0530

    added the set

---
+++ set/Demo3.java
@@ -0,0 +1,17 @@
+package set;
+
+import java.util.TreeMap;
+
+public class Demo3 {
+    public static void main(String[] args){
+        TreeMap<Integer,String>map=new TreeMap<>();
+        map.put(101, "Aditya");
+        map.put(102, "Abhishek");
+        map.put(103, "Raj");
+        // System.out.println(map.firstKey());
+        // System.out.println(map.lastKey());
+        // System.out.println(map.headMap(102));
+        // System.out.println(map.tailMap(103));
+    }
+    
+}
